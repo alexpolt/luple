c96a308bd00719bf4ca44c34f3deb2fed60f2ba2
Alexandr Poltavsky <alexpolt@yandex.ru>
2017-10-23 04:30:58 +0300

    added as_nuple( python2.7(...), value, ... )

--- nuple.h
+++ nuple.h
@@ -21,19 +21,34 @@ Usage:
 
   #include "nuple.h"
 
-  using nameid_t = nuple< $("name"), char const*, $("id"), int >; //$(...) - see intern.h header
+  using nameid_t = nuple< $("name"), char const*, $("id"), int >; //for $(...) - see intern.h header
+
 
   nameid_t n[] = { {"alex", 1}, {"ivan", 2} };
 
   for( auto const& v : n )
+
     printf( "name: %s, id: %d\n", get<$("name")>(v), get<$("id")>(v) );
 
 
   auto get_person ( int i ) { return nameid_t{ "john", i }; }
 
+  //or - return nuple<$("name"), char const*, $("id"), int>{ "john", i };
+
+  //or - return as_nuple( $name("name"), "john", $name("id"), i ); 
+
+
   auto p = get_person( 3 );
 
   printf( "name: %s, id: %d\n", get<$("name")>( p ), get<$("id")>( p ) );
+
+
+  //as_nuple(...), similar to make_tuple, macro is $name(...) instead of $(...)
+
+  auto get_person( int id ) {
+
+    return as_nuple( $name("name"), std::string{ "john" }, $name("id"), id ); 
+  }
 
 
   //nuple extends luple, so luple methods work as usual (as in tuple)
@@ -48,6 +63,11 @@ Usage:
 
   //see luple.h for more examples
 
+
+  // nuple_ns::nuple_t - return type for tag name by index
+
+  using field0_t = nuple_ns::name_t< nameid_t, 0 >; //the same as $("name")
+
 */
 
 
@@ -57,32 +77,31 @@ Usage:
 
 namespace nuple_ns {
 
+
   //filter template sorts nuple parameters into two lists: 
   //types (passed to luple) and names (used for looking up member index)
 
-  template<typename TL, typename NL, typename... TT> struct filter_ {
+  template<typename TL, typename NL, int is_str, typename... TT> struct filter_;
+
+  template<typename TL, typename NL, typename T, typename... TT> 
+  struct filter_< TL, NL, -1, T, TT... > : 
+    filter_< TL, NL, intern::is_string< T >::value, T, TT... > {};
+
+  template<typename TL, typename NL, typename T, typename... TT> 
+  struct filter_< TL, NL, true, T, TT... > : 
+    filter_< TL, typename NL::template add<T>::type, -1, TT... > {};
+
+  template<typename TL, typename NL, typename T, typename... TT> 
+  struct filter_< TL, NL, false, T, TT... > : 
+    filter_< typename TL::template add<T>::type, NL, -1, TT... > {};
+
+  template<typename TL, typename NL, int is_str> struct filter_< TL, NL, is_str > {
     using nlist = NL;
     using tlist = TL;
   };
 
-  template<typename... TL, typename... NL, typename... TT, char... NN>
-  struct filter_< luple_ns::type_list<TL...>, luple_ns::type_list<NL...>, intern::string<NN...>, TT... > {
-    using tl = luple_ns::type_list<TL...>;
-    using nl = luple_ns::type_list<NL..., intern::string<NN...>>;
-    using nlist = typename filter_< tl, nl, TT... >::nlist;
-    using tlist = typename filter_< tl, nl, TT... >::tlist;
-  };
-
-  template<typename... TL, typename... NL, typename T, typename... TT>
-  struct filter_< luple_ns::type_list<TL...>, luple_ns::type_list<NL...>, T, TT... > {
-    using tl = luple_ns::type_list<TL..., T>;
-    using nl = luple_ns::type_list<NL...>;
-    using nlist = typename filter_< tl, nl, TT... >::nlist;
-    using tlist = typename filter_< tl, nl, TT... >::tlist;
-  };
-
   template<typename... TT>
-  using filter = filter_< luple_ns::type_list<>, luple_ns::type_list<>, TT... >;
+  using filter = filter_< luple_ns::type_list<>, luple_ns::type_list<>, -1, TT... >;
 
 
   //nuple is just a thin layer on top of luple
@@ -101,7 +120,7 @@ namespace nuple_ns {
   //turn a nuple member name into an index and call get< index >( ... )
 
   template<typename T, typename = std::enable_if_t< intern::is_string<T>::value >, typename... TT> 
-  constexpr auto& get ( nuple<TT...>& t ) {
+  constexpr auto & get ( nuple<TT...> & t ) {
 
     static_assert( luple_ns::tlist_get_n< typename nuple<TT...>::name_list, T>::value != -1, "no such nuple name" );
 
@@ -109,20 +128,69 @@ namespace nuple_ns {
   }
 
   template<typename T, typename = std::enable_if_t< intern::is_string<T>::value >, typename... TT> 
-  constexpr auto& get ( nuple<TT...> const& t ) {
+  constexpr auto & get ( nuple<TT...> const & t ) {
 
     static_assert( luple_ns::tlist_get_n< typename nuple<TT...>::name_list, T>::value != -1, "no such nuple name" );
     
     return get< luple_ns::tlist_get_n< typename nuple<TT...>::name_list, T >::value >( t ); 
   }
 
+
   //nuple member name (interned string) for index
   template<typename T, int N>
   using name_t = luple_ns::tlist_get_t< typename T::name_list, N >;
 
+
+  //as_nuple( ... )
+
+  #define $name(s) $(s){}
+
+  template<typename T, int N = 0, typename = void> 
+  struct check_args {
+
+    static const bool value = false;
+  };
+
+  template<typename T, typename... TT, int N>
+  struct check_args< luple_ns::type_list<T, TT...>, N,
+
+          std::enable_if_t< ( N%2 > 0 ) || ( N%2 == 0 && intern::is_string<T>::value ) >> {
+
+    static const bool value = true && check_args< luple_ns::type_list<TT...>, N + 1 >::value;
+  };
+
+  template<int N> 
+  struct check_args< luple_ns::type_list<>, N, void> {  
+
+    static const bool value = true;
+  };
+
+
+  template<typename... TT, int... NN> 
+  auto as_nuple_( luple<TT...> l, std::integer_sequence< int, NN... > ) {
+
+    using param_list = luple_ns::type_list<TT...>;
+
+    static_assert( check_args<param_list>::value, "order of arguments should be name, value..." );
+
+    return nuple< TT... >{ std::move( get<NN*2+1>( l ) )... };
+  }
+  
+  //as_nuple( $name("..."), value, ... ) -> nuple< $name(...), decltype(value), ... >
+
+  template<typename... TT>
+  auto as_nuple( TT... args ) {
+
+    static_assert( sizeof...(TT) % 2 == 0, "wrong number of arguments");
+
+    return as_nuple_( luple< TT... >{ std::move( args )... }, std::make_integer_sequence< int, sizeof...(TT)/2 >{} );
+  }
+
 }
 
+//import into global namespace
 
 using nuple_ns::nuple;
+using nuple_ns::as_nuple;
 
 
